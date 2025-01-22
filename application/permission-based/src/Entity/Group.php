95fdc111dabc1a5adf0146d77b25a98828d644b2
giedrius <giedrius@inkodus.lt>
2025-01-22 22:53:53 +0200

    Creating groups table.

---
+++ application/permission-based/src/Entity/Group.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Kukulis\PermissionBased\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Kukulis\PermissionBased\Repository\GroupRepository;
+
+#[ORM\Entity(repositoryClass: GroupRepository::class)]
+#[ORM\Table(name: '`groups`')]
+class Group
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
