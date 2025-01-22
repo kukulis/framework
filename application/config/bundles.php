4e91226f6ed39a40dc3fead4bcdbd2ec191f0237
giedrius <giedrius@inkodus.lt>
2025-01-22 15:23:42 +0200

    Docker container for database created. Doctrine lib introduced.

--- application/config/bundles.php
+++ application/config/bundles.php
@@ -5,4 +5,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
