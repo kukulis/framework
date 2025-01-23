945da41b7a1bbbf893a68cd26d9590c6f27ac29a
giedrius <giedrius@inkodus.lt>
2025-01-23 15:23:16 +0200

    Created check permission listener skeleton. Installed monolog.

--- application/kukulis-permissionbased/src/Listeners/CheckPermissionRightListener.php
+++ application/kukulis-permissionbased/src/Listeners/CheckPermissionRightListener.php
@@ -3,19 +3,32 @@
 namespace Kukulis\PermissionBased\Listeners;
 
 use Kukulis\PermissionBased\Repository\PermissionRepository;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 
 class CheckPermissionRightListener
 {
 
     public function __construct(
-        PermissionRepository $permissionRepository
+        private PermissionRepository $permissionRepository,
+        private Security $security,
+        private LoggerInterface $logger
     )
     {
     }
 
     public function __invoke(RequestEvent $event): void
     {
+        $user = $this->security->getUser();
+
+        if ( $user === null ) {
+            // let unauthenticated user is handled by a default way.
+            return;
+        }
+
+        $pathInfo = $event->getRequest()->getPathInfo();
+        $this->logger->error('CheckPermissionRightListener pathInfo='.$pathInfo);
 
     }
 }
